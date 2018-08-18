d71a299b5d584f272821e9a9a4651e821418d0e0
Douglas Christopher Wilson <doug@somethingdoug.com>
2018-08-18 15:08:28 -0400

    lint: apply standard 11 style

--- index.js
+++ index.js
@@ -6,21 +6,21 @@
  * MIT Licensed
  */
 
-'use strict';
+'use strict'
 
 /**
  * Module variables.
  * @private
  */
 
-var matchHtmlRegExp = /["'&<>]/;
+var matchHtmlRegExp = /["'&<>]/
 
 /**
  * Module exports.
  * @public
  */
 
-module.exports = escapeHtml;
+module.exports = escapeHtml
 
 /**
  * Escape special characters in the given string of html.
@@ -30,49 +30,49 @@ module.exports = escapeHtml;
  * @public
  */
 
-function escapeHtml(string) {
-  var str = '' + string;
-  var match = matchHtmlRegExp.exec(str);
+function escapeHtml (string) {
+  var str = '' + string
+  var match = matchHtmlRegExp.exec(str)
 
   if (!match) {
-    return str;
+    return str
   }
 
-  var escape;
-  var html = '';
-  var index = 0;
-  var lastIndex = 0;
+  var escape
+  var html = ''
+  var index = 0
+  var lastIndex = 0
 
   for (index = match.index; index < str.length; index++) {
     switch (str.charCodeAt(index)) {
       case 34: // "
-        escape = '&quot;';
-        break;
+        escape = '&quot;'
+        break
       case 38: // &
-        escape = '&amp;';
-        break;
+        escape = '&amp;'
+        break
       case 39: // '
-        escape = '&#39;';
-        break;
+        escape = '&#39;'
+        break
       case 60: // <
-        escape = '&lt;';
-        break;
+        escape = '&lt;'
+        break
       case 62: // >
-        escape = '&gt;';
-        break;
+        escape = '&gt;'
+        break
       default:
-        continue;
+        continue
     }
 
     if (lastIndex !== index) {
-      html += str.substring(lastIndex, index);
+      html += str.substring(lastIndex, index)
     }
 
-    lastIndex = index + 1;
-    html += escape;
+    lastIndex = index + 1
+    html += escape
   }
 
   return lastIndex !== index
     ? html + str.substring(lastIndex, index)
-    : html;
+    : html
 }
