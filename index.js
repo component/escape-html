f1f45c3e987a2c073482433be101478618116993
Josh Goebel <me@joshgoebel.com>
2020-11-23 21:00:17 -0500

    wip

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ var matchHtmlRegExp = /["'&<>]/
  * @public
  */
 
-module.exports = escapeHtml
+module.exports = { escapeHtml, escapeHtmlFast, escapeHtmlNoRegex }
 
 /**
  * Escape special characters in the given string of text.
@@ -29,6 +29,86 @@ module.exports = escapeHtml
  * @return {string}
  * @public
  */
+
+var matchHtmlRegExpFast = /["'&<>]/g
+function escapeHtmlFast (str) {
+  var lastIndex = 0
+  var html = ''
+  var escape = ''
+  var match
+
+  while (match = matchHtmlRegExpFast.test(str)) {
+    switch (str.charCodeAt(matchHtmlRegExpFast.lastIndex)) {
+      case 34: // "
+        escape = '&quot;'
+        break
+      case 38: // &
+        escape = '&amp;'
+        break
+      case 39: // '
+        escape = '&#39;'
+        break
+      case 60: // <
+        escape = '&lt;'
+        break
+      case 62: // >
+        escape = '&gt;'
+        break
+    }
+    // console.log(lastIndex, matchHtmlRegExpFast.lastIndex, str.length)
+    html += str.substring(lastIndex, matchHtmlRegExpFast.lastIndex)
+    lastIndex = matchHtmlRegExpFast.lastIndex + 1
+    html += escape
+  }
+  return html + str.substring(lastIndex)
+}
+
+function escapeHtmlNoRegex (str) {
+  // var str = '' + string
+  // var match = matchHtmlRegExp.exec(str)
+
+  // if (!match) {
+  //   return str
+  // }
+
+  var escape
+  var html = ''
+  var index = 0
+  var lastIndex = 0
+
+  for (index = 0; index < str.length; index++) {
+    switch (str.charCodeAt(index)) {
+      case 34: // "
+        escape = '&quot;'
+        break
+      case 38: // &
+        escape = '&amp;'
+        break
+      case 39: // '
+        escape = '&#39;'
+        break
+      case 60: // <
+        escape = '&lt;'
+        break
+      case 62: // >
+        escape = '&gt;'
+        break
+      default:
+        continue
+    }
+
+    if (lastIndex !== index) {
+      html += str.substring(lastIndex, index)
+    }
+
+    lastIndex = index + 1
+    html += escape
+  }
+
+  return lastIndex !== index
+    ? html + str.substring(lastIndex, index)
+    : html
+}
 
 function escapeHtml (string) {
   var str = '' + string
